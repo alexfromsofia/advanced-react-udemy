d6496a889a813f503bbc8bbe7d1d64491be243c7
Alex <alexander@tradeo.com>
2018-11-15 12:05:54 +0200

    Test CommentBox

--- src/components/__tests__/CommentBox.test.js
+++ src/components/__tests__/CommentBox.test.js
@@ -14,9 +14,27 @@ afterEach(() => {
 });
 
 it('should display a textarea and a button', () => {
-
-
   expect(wrapped.find('textarea').length).toEqual(1);
   expect(wrapped.find('button').length).toEqual(1);
 });
 
+describe('textarea', () => {
+  beforeEach(() => {
+    wrapped.find('textarea').simulate('change', {
+      target: { value: 'new comment' }
+    });
+    wrapped.update();
+  });
+
+  it('has a textarea that users can type in', () => {
+    expect(wrapped.find('textarea').prop('value')).toEqual('new comment');
+  });
+
+  it('should clear textarea after submit', () => {
+    wrapped.find('form').simulate('submit');
+    wrapped.update();
+
+    expect(wrapped.find('textarea').prop('value')).toEqual('');
+  });
+
+});
